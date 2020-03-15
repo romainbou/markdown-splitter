d1d058eac6b4d2d4ee2e792e23e02503e2903fc0
Romain Bouyé <romain@bouye.fr>
2020-03-15 17:55:27 +0000

    Inital commit: Save the content of the first occurence of the export tag in a file export.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Markdown Splitter
+
+Utiliy tool to split a Markdown file into chunks following annotations
+
+## Annotation syntax
+
+In order to identify which part of the document should be splitted we can use 'export' open and closing tags
+
+```markdown
+# This is a document the is going to be splitted
+
+<!-- export -->
+This unnamed part will generate a new `export.md`
+<!-- /export -->
+
+This part will be ignored
+
+<!-- export part1 -->
+This contents will generate a new `part1.md`
+<!-- /export part1 -->
+
+<!-- export part-two.md -->
+This contents will generate a new `part-two.md`
+<!-- /export -->
+
+```
+
+# Usage
+
+```bash
+mds markdown.md
+```
+
+```bash
+tree
+
+```
+
+# Options
+
+```bash
+mds -v markdown.md
+export.md created
+part1.md created
+part-two.md created
+```
+
+# Use Case
+Maintaining a diary or a blog in one file with annotations. This tool allows to split the file into multiple markdown files for each articles. Those markdown articles are now easy to publish with static site generators like Gatsby.
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn write_file(filename: &str, contents: String) -> std::io::Result<()> {
+    let mut file = File::create(filename).expect("Unable to create file");
+    file.write_all(contents.as_bytes())
+}
+
+fn read_file(filename: &str) -> String {
+    let mut file = File::open(filename).expect("Unable to open");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Couldn't read file");
+    return contents;
+}
+
+// Returns line number of the first occurence of a pattern in a string
+// -1 otherwive
+fn find_pattern(pattern: String, text: &mut String, start: i32, end: i32) -> i32 {
+
+    for (line_number, line) in text.as_str().lines().enumerate() {
+        let line_number = line_number as i32;
+        let line_number = line_number + 1; // Line count starts by 1
+        let is_pattern_found = line.find(&pattern);
+        if is_pattern_found != None {
+            if (start == -1 || line_number >= start) && (end == -1 || line_number <= end) {
+                return line_number;
+            }
+        }
+    }
+
+    return -1;
+}
+
+fn split_text(text: &mut String, start: i32, end: i32) -> String{
+    let mut sub_text: String = "".to_string();
+    for (line_number, line) in text.as_str().lines().enumerate() {
+        let line_number = line_number as i32;
+        let line_number = line_number + 1; // Line count starts by 1
+        if line_number > start && line_number < end {
+            sub_text = sub_text + line;
+        }
+    }
+    return sub_text;
+}
+
+fn export_by_pattern(pattern: String, text: &mut String) -> String{
+
+    let mut text = text.clone();
+
+    let mut full_pattern_start = String::from("<!-- export -->");
+    let mut full_pattern_end = String::from("<!-- /export -->");
+    if pattern != "".to_string() {
+        full_pattern_start = format!("{}{}{}", String::from("<!-- export "), pattern, String::from(" -->"));
+        full_pattern_end = format!("{}{}{}", String::from("<!-- /export "), pattern, String::from(" -->"));
+    }
+
+    let line1 = find_pattern(full_pattern_start, &mut text, -1, -1);
+    let line2 = find_pattern(full_pattern_end, &mut text, -1, -1);
+    let sub_text = split_text(&mut text, line1, line2);
+    return sub_text;
+}
+
+fn main() {
+
+    let mut file_contents = read_file("README.md");
+    let sub_text = export_by_pattern("".to_string(), &mut file_contents);
+
+    write_file("export.md", sub_text).unwrap();
+
+}
